4ec548e51cca731705c2d7747b56d857ebeada91
Bartek Krupa <krupa1.bk@gmail.com>
2020-04-12 14:18:09 +0200

    Updates

---
+++ precache-manifest.a77b4392aef59e21ccdf9e33a5826d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa407829325be9585b4a07a90f96995f",
+    "url": "/bartoszkrupa/index.html"
+  },
+  {
+    "revision": "6bd23ce45131ce9570f7",
+    "url": "/bartoszkrupa/static/css/main.a9b1bda2.chunk.css"
+  },
+  {
+    "revision": "4a87602a69e1795f44a5",
+    "url": "/bartoszkrupa/static/js/2.930bd101.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bartoszkrupa/static/js/2.930bd101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd23ce45131ce9570f7",
+    "url": "/bartoszkrupa/static/js/main.f9008611.chunk.js"
+  },
+  {
+    "revision": "cab06a04781a19d0f326",
+    "url": "/bartoszkrupa/static/js/runtime-main.8d337b76.js"
+  }
+]);
